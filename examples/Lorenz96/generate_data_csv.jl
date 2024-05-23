76628e3feee02d6eba6f05024f3f6f0cc60e6a65
achiang17 <andrewchiang17@gmail.com>
2024-05-23 10:32:08 -0700

    results for MSE and NRMSE of both forcing levels

--- examples/Lorenz96/generate_data_csv.jl
+++ examples/Lorenz96/generate_data_csv.jl
@@ -9,13 +9,13 @@ function lorenz96(du,u,p,t)
 end
 
 n = 5
-F = 0.9
+F = 8
 dt = 0.01
 trajectory_length = 2000
 tspan = (0.0, trajectory_length * dt)
 num_csvs = 128
 
-group = "train"
+group = "test8"
 for i in 1:num_csvs
     u0 = F .+ 0.5 .* (2 .* rand(n) .- 1)
     prob = ODEProblem(lorenz96,u0,tspan,F)
--- examples/Lorenz96/lorenz96_task.py
+++ examples/Lorenz96/lorenz96_task.py
@@ -29,7 +29,7 @@ parser.add_argument('--log_interval', type=int, default=100,
 #   help='size of test set')
 parser.add_argument('--lr', type=float, default=2e-2,
     help='learning rate')
-parser.add_argument('--dt',type=float, default=6e-2,
+parser.add_argument('--dt',type=float, default=1e-2,
     help='step size <dt> of the coRNN')
 parser.add_argument('--gamma',type=float, default=66,
     help='y controle parameter <gamma> of the coRNN')
@@ -45,21 +45,24 @@ model = model.coRNN(n_inp, args.n_hid, n_out, args.dt, args.gamma, args.epsilon)
 
 
 
-objective = nrmse.NRMSELoss()
+objective = nn.MSELoss()
 optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
 def test(path_to_test_csv):
     model.eval()
     with torch.no_grad():
         data, label = utils.get_data(path_to_test_csv, args.T_in, args.T_pred)
-        #label = label.unsqueeze(1)
         out = model(data.to(device))
         loss = objective(out, label.to(device))
+        rmse_loss = torch.sqrt(loss)
+        rms_true = torch.sqrt(torch.mean(label ** 2))
+        nrmse_loss = rmse_loss/rms_true
+        
     return loss.item()
 
 
 def train():
-    train_dir = 'train/'
+    train_dir = 'train8/'
     files = os.listdir(train_dir)
     num_files = len(files)
 
@@ -67,18 +70,25 @@ def train():
     steps = []
     
     for i in range(1,num_files+1):
-        data, label = utils.get_data(f"train/train_{i}.csv", args.T_in, args.T_pred)
+        data, label = utils.get_data(f"train8/train8_{i}.csv", args.T_in, args.T_pred)
+        # print(f"data: {data}")
+        # print(f"data.size(): {data.size()}")
+        # print(f"label: {label}")
+        # print(f"label.size(): {label.size()}")
         optimizer.zero_grad()
         out = model(data.to(device))
         loss = objective(out, label.to(device))
+        rmse_loss = torch.sqrt(loss)
+        rms_true = torch.sqrt(torch.mean(label ** 2))
+        nrmse_loss = rmse_loss/rms_true
         loss.backward()
         optimizer.step()
 
         if(i%1==0 and i!=0):
-            mse_error = test(f"test/test_{i}.csv")
+            error = test(f"test8/test8_{i}.csv")
             #print('Test NRMSE: {:.6f}'.format(mse_error))
             steps.append(i)
-            test_err.append(mse_error)
+            test_err.append(error)
             model.train()
 
     return steps,test_err
@@ -86,10 +96,15 @@ def train():
 
 if __name__ == '__main__':
     steps,test_err = train()
+    # for param in model.parameters():
+    #     print("new param \n")
+    #     print(f"param.size : {param.size()}")
+    #     print(param)
+    #     print("\n\n")
     #print(f"test_mse: {test_mse}")
-    #print(mean(test_err[100:]))
+    print(mean(test_err[100:]))
     plt.plot(steps,test_err)
-    plt.title("lorenz96: NRMSE vs Training steps")
-    plt.ylabel("NRMSE")
+    plt.title("lorenz96: MSE vs Training steps, F = 8.0")
+    plt.ylabel("MSE")
     plt.xlabel("Training steps")
-    plt.savefig("NRMSE_vs_steps_lorenz.png")
+    plt.savefig("MSE_vs_steps_lorenz8.png")
